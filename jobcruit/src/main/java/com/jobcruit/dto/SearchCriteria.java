34dca5da4da986b7a362e195b0fa0eee045bffdb
DESKTOP-S5U860B\NahyunWon <uranus413@gmail.com>
2017-11-10 16:41:33 +0900

    SearchCriteria 수정함. 짠지 미안...

--- jobcruit/src/main/java/com/jobcruit/dto/SearchCriteria.java
+++ jobcruit/src/main/java/com/jobcruit/dto/SearchCriteria.java
@@ -6,21 +6,5 @@ import lombok.Data;
 public class SearchCriteria extends Criteria{
 
 	private String searchType;
-	private String searchKeyword;
-	
-	
-	@Override
-	public String toString() {
-		return super.toString() + "SearchCriteria" + "[searchType=" + searchType + 
-				", searchKeyword=" + searchKeyword + "]";
-	}
-	@Override
-	public int getSkip() {
-		return super.getSkip();
-	}
-	@Override
-	public int getSize() {
-		return super.getSize();
-	}
-	
+	private String keyword;
 }
