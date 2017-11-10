acc4bf8ea377755638b4c77ea949ee2c7696d5f4
DESKTOP-M3H2OM6\user <hyunjoon512@naver.com>
2017-11-10 16:23:48 +0900

    SearchCriteria

--- jobcruit/src/main/java/com/jobcruit/dto/SearchCriteria.java
+++ jobcruit/src/main/java/com/jobcruit/dto/SearchCriteria.java
@@ -6,13 +6,21 @@ import lombok.Data;
 public class SearchCriteria extends Criteria{
 
 	private String searchType;
-	private String keyword;
+	private String searchKeyword;
 	
 	
 	@Override
 	public String toString() {
 		return super.toString() + "SearchCriteria" + "[searchType=" + searchType + 
-				", keyword=" + keyword + "]";
+				", searchKeyword=" + searchKeyword + "]";
+	}
+	@Override
+	public int getSkip() {
+		return super.getSkip();
+	}
+	@Override
+	public int getSize() {
+		return super.getSize();
 	}
 	
 }
