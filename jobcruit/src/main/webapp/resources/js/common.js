df794b7a3a353b227f6874e0bc64693c375d471f
DESKTOP-S5U860B\NahyunWon <uranus413@gmail.com>
2017-11-13 10:34:38 +0900

    common.js 추가

--- jobcruit/src/main/webapp/resources/js/common.js
+++ jobcruit/src/main/webapp/resources/js/common.js
@@ -12,3 +12,33 @@ function makePage(param){
     prev = first != 1? true:false;
     return {first:first, last:last, next:next, prev:prev, page:param.page};
 }
+
+function getDateFormat(date,sep){
+	var d = new Date(date);
+	var month = d.getMonth()+1;
+	var date = d.getDate();
+	
+	return d.getFullYear()+sep+(month < 10 ? "0"+month:month)+sep+(date < 10 ? "0"+date:date);
+}
+
+function getDateTime(date){
+	var d = new Date(date);
+	var month = d.getMonth()+1;
+	var date = d.getDate();
+	var seconds = d.getSeconds();
+	
+	return d.getFullYear()+"-"+(month < 10 ? "0"+month:month)+"-"+(date < 10 ? "0"+date:date)+" "+d.getHours()+":"+d.getMinutes()+":"+(seconds < 10 ?"0"+seconds:seconds);
+}
+
+function showAlert(msg, type, isButton, time){
+	type = type ? type : "success";
+	isButton = isButton ? isButton : false;
+	time = time ? time : 1000;
+	
+	swal({
+		title:msg,
+		type:type,
+		showConfirmButton: isButton,
+		timer: time
+	});
+}
